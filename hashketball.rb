1c35db3a1a45d353be351fed606ac802f5dbc66d
Kara O <karajo.odle@gmail.com>
2019-05-27 18:24:39 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -30,7 +30,8 @@ def num_points_scored(player_name)
 end
 
 def shoe_size(player_name)
-  
+  game_hash[:home][:players].include?(player_name) ? game_hash[:home][:players][player_name][:shoe] : game_hash[:away][:players][player_name][:shoe]
+end
 
 
 
